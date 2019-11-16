912813ecb5c236e287a80f421865ae0200ec513f
Vitor Nicacio <vitornicacio10@hotmail.com>
2019-11-16 14:51:26 -0300

    script

---
+++ SqlUtilizado.sql
@@ -0,0 +1,4 @@
+﻿  insert into AspNetRoles VALUES(NEWID(),'Membro')
+  insert into AspNetUserRoles VALUES('4260c7bf-a88b-40a1-95cd-023188c34b7f', '449733AE-79E8-485B-9ED9-FD8B1A3BE15C')
+
+  -- necessário incluir novamente o regra e a associação
